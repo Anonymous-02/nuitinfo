696b52903a4112f7649618a024d5ca5367029b97
remi <rozierrremi@gmail.com>
2024-12-06 00:00:57 +0100

    remove unused

--- scroll.js
+++ scroll.js
@@ -1,13 +1,4 @@
 window.onload = function () {
-  var wheelDistance = function (evt) {
-    if (!evt) evt = event;
-    var w = evt.wheelDelta,
-      d = evt.detail;
-    if (d) {
-      if (w) return (w / d / 40) * d > 0 ? 1 : -1; // Opera
-      else return -d / 3; // Firefox;         TODO: do not /3 for OS X
-    } else return w / 120; // IE/Safari/Chrome TODO: /3 for Chrome OS X
-  };
   var wheelDirection = function (evt) {
     if (!evt) evt = event;
     return evt.detail < 0 ? 1 : evt.wheelDelta > 0 ? 1 : -1;
