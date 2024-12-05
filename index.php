0df8c3e9d5bc9c9cd1842d779f62851bb10e0218
remi <rozierrremi@gmail.com>
2024-12-05 21:16:08 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+echo('
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>╠╬═╦╩</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="index.js"></script>
+  </head>
+  <body>
+    <img src="corps.jpeg" id="myImgId" />
+    <p>X:<span id="x"></span></p>
+    <p>Y:<span id="y"></span></p>
+    <script type="text/javascript">
+      let myImg = document.getElementById("myImgId");
+      myImg.onmousedown = GetCoordinates;
+    </script>
+  </body>
+</html>')
