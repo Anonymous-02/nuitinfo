c9c1ce17d02a3fb56ab97de0ff1e1a54e47c2e04
remi <rozierrremi@gmail.com>
2024-12-06 00:00:27 +0100

    correct scroll

--- scroll.js
+++ scroll.js
@@ -15,32 +15,20 @@ window.onload = function () {
   var test = document.getElementById("test");
   var results = document.getElementById("results");
   function showResults(evt) {
+    console.log("scroll");
     var direction = wheelDirection(evt);
     if (direction > 0) {
       if (document.URL.includes("paralleles.html"))
-        location.href = document.document.URL.replace(
-          "paralleles.html",
-          "index.html"
-        );
+        location.href = document.URL.replace("paralleles.html", "index.html");
       if (document.URL.includes("ocean.html"))
-        location.href = document.document.URL.replace(
-          "ocean.html",
-          "paralleles.html"
-        );
+        location.href = document.URL.replace("ocean.html", "paralleles.html");
     } else {
       if (document.URL.includes("paralleles.html"))
-        location.href = document.document.URL.replace(
-          "paralleles.html",
-          "ocean.html"
-        );
+        location.href = document.URL.replace("paralleles.html", "ocean.html");
       if (document.URL.includes("index.html"))
-        location.href = document.document.URL.replace(
-          "index.html",
-          "paralleles.html"
-        );
+        location.href = document.URL.replace("index.html", "paralleles.html");
     }
   }
-
   if (test.addEventListener) {
     test.addEventListener("mousewheel", showResults, false); // Chrome/Safari/Opera
     test.addEventListener("DOMMouseScroll", showResults, false); // Firefox
